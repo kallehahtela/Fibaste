447ee7e2b5e94ee81324d948b63a7911eb6af690
Kalle Hahtela <khahtela@gmail.com>
2024-08-17 21:59:40 +0300

    Console log optimations.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'This message is coming from the server' });
 });
 
-// demonstrating
+
 app.listen(8000, () => {
-    console.log('The app is running on http://localhost:8000');
+    console.log('The app is running on site http://localhost:8000');
 });
