bcb8eafe0f82fc90831ff827c9c1d582e4ed9d37
Kalle Hahtela <khahtela@gmail.com>
2024-08-17 21:53:28 +0300

    Fibaste Project Setup & Basic Express Server.

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+
+// listening the home route
+app.get('/', (req, res) => {
+    res.json({ message: 'This message is coming from the server' });
+});
+
+// demonstrating
+app.listen(8000, () => {
+    console.log('The app is running on http://localhost:8000');
+});
