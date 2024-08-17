1972c27b9645d9fd7ea5f5dff44318fa88b9b073
Kalle Hahtela <khahtela@gmail.com>
2024-08-17 22:42:02 +0300

    Post Request & Reading Incoming Data.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.json({ message: 'This message is coming from the server' });
 });
 
+app.post('/', (req, res) => {
+    req.on('data', (chunk) => {
+        console.log(JSON.parse(chunk));
+        console.log(req.body);
+        res.json({ message: 'This message is coming from the post request' })
+    });
+});
+
 
 app.listen(8000, () => {
     console.log('The app is running on site http://localhost:8000');
